d948f9789c89ff348ca1ce61564f84b5ac3f3a60
Vikas Lakka <vikaslakkacs@gmail.com>
2022-05-10 13:45:55 +0530

    Added Logging Parameters

--- or.py
+++ or.py
@@ -5,7 +5,7 @@ import logging
 import os
 
 logging.basicConfig(
-    filename=os.path.join('logs','runing_logs.log'),
+    filename=os.path.join('logs','running_logs.log'),
     level=logging.INFO,
     format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
     filemode='a'
